3d8b6722204b508609c63c7e3ac2cbba4fc76460
Andrew W <andrew.whelan@ucdconnect.ie>
2018-07-13 23:12:17 +0100

    Adding linGeomVelocity and linGeomPressureVelocity solid models

---
+++ src/solids4FoamModels/solidModels/linGeomPressureVelocitySolid/linGeomPressureVelocitySolid.C
@@ -0,0 +1,298 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright held by original author
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software; you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM; if not, write to the Free Software Foundation,
+    Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+\*---------------------------------------------------------------------------*/
+
+#include "linGeomPressureVelocitySolid.H"
+#include "fvm.H"
+#include "fvc.H"
+#include "fvMatrices.H"
+#include "addToRunTimeSelectionTable.H"
+#include "linearElastic.H"
+#include "findRefCell.H"
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace Foam
+{
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace solidModels
+{
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+defineTypeNameAndDebug(linGeomPressureVelocitySolid, 0);
+addToRunTimeSelectionTable(physicsModel, linGeomPressureVelocitySolid, solid);
+addToRunTimeSelectionTable(solidModel, linGeomPressureVelocitySolid, dictionary);
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+linGeomPressureVelocitySolid::linGeomPressureVelocitySolid
+(
+    Time& runTime,
+    const word& region
+)
+:
+    solidModel(typeName, runTime, region),
+    impK_(mechanical().impK()),
+    impKf_(mechanical().impKf()),
+    rImpK_(1.0/impK_),
+    gradU_(fvc::grad(U())),
+    p_
+    (
+        IOobject
+        (
+            "p",
+            runTime.timeName(),
+            mesh(),
+            IOobject::MUST_READ,
+            IOobject::AUTO_WRITE
+        ),
+        mesh()
+    ),
+    mu_
+    (
+        IOobject
+        (
+            "mu",
+            runTime.timeName(),
+            mesh(),
+            IOobject::NO_READ,
+            IOobject::NO_WRITE
+        ),
+        mesh(),
+        dimensionedScalar("0", dimPressure, 0.0)
+    ),
+    muf_
+    (
+        IOobject
+        (
+            "interpolate(mu)",
+            runTime.timeName(),
+            mesh(),
+            IOobject::NO_READ,
+            IOobject::NO_WRITE
+        ),
+        mesh(),
+        dimensionedScalar("0", dimPressure, 0.0)
+    ),
+    lambdaf_
+    (
+        IOobject
+        (
+            "interpolate(lambda)",
+            runTime.timeName(),
+            mesh(),
+            IOobject::NO_READ,
+            IOobject::NO_WRITE
+        ),
+        mesh(),
+        dimensionedScalar("0", dimPressure, 0.0)
+    ),
+    kf_
+    (
+        IOobject
+        (
+            "k",
+            runTime.timeName(),
+            mesh(),
+            IOobject::NO_READ,
+            IOobject::NO_WRITE
+        ),
+        mesh(),
+        dimensionedScalar("0", dimPressure, 0.0)
+    ),
+currentRho_(rho())
+
+
+{    // Cast the mechanical law to a linearElastic mechanicalLaw
+const PtrList<mechanicalLaw>& mechLaws = mechanical();
+    const linearElastic& mech = refCast<const linearElastic>(mechLaws[0]);
+
+    // Set mu and lambda fields
+    mu_ = mech.mu();
+    muf_ = mech.mu();
+    //muf_ = fvc::interpolate(mu_);
+    lambdaf_ = mech.lambda(); 
+    kf_=mech.K();
+    p_.oldTime();
+}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+
+bool linGeomPressureVelocitySolid::evolve()
+{
+//nICorr_(solidProperties().lookupOrDefault<int>("nInnerCorrectors", 3))
+    const int nICorr_ = readInt(solidProperties().lookup("nInnerCorrectors"));
+    label pRefCell = 0;
+    scalar pRefValue = 0.0;
+    setRefCell(p_, solidProperties(), pRefCell, pRefValue);
+ 
+//Momentum
+
+    //volScalarField& rho1 = rho();
+    //const volScalarField& rho1 = rho();
+
+
+//    const int nICorr = readInt(solidProperties().lookupOrDefault("nInnerCorrectors", 3));
+
+
+
+for (int oCorr = 0; oCorr < nCorr(); oCorr++)
+{
+
+U().storePrevIter();
+
+fvVectorMatrix UEqn
+
+(
+    fvm::ddt(currentRho_, U())
+    //fvm::ddt(rho(), U())
+    -fvm::laplacian(2*muf_*runTime().deltaT(), U())
+    //+fvc::div(2*muf_*runTime().deltaT(), fvc::grad(U()))
+    +fvc::div(2*mu_*runTime().deltaT()*gradU_)
+    -fvc::div(dev(sigma()))
+);
+
+solve(UEqn==-fvc::grad(p_));
+//UEqn.solve();
+
+//Info << U() << endl;
+
+volScalarField rAU=1/UEqn.A();
+
+
+
+       for (int iCorr = 0; iCorr < nICorr_; iCorr++)
+       //Start of Piso Loop
+           {
+           p_.storePrevIter();
+
+           fvScalarMatrix PEqn
+           (
+           fvm::ddt(rho()/kf_, p_)
+           //+fvc::div(rho(), UEqn.H())
+           +fvc::div(rho()*rAU*UEqn.H())
+           +fvc::div(((rho()*rAU)/kf_)*UEqn.H()*p_)
+           //-fvm::laplacian(currentRho_*rAU, p_)
+           ==
+           fvm::laplacian(currentRho_*rAU, p_)
+           );
+
+
+
+//           fvScalarMatrix PEqn
+//           (
+//           fvm::ddt(rho()/kf_, p_)
+//           +fvm::div((rho()/kf_), U()*p_)
+//           ==
+//           -fvc::div(rho()*U())
+//           );
+
+
+
+           PEqn.setReference(pRefCell, pRefValue); 
+        PEqn.relax();
+
+           PEqn.solve();
+           //solve(PEqn==0);
+
+           U() = UEqn.H()*rAU-rAU*fvc::grad(p_);
+
+          // currentRho_ = rho()*(1 + (p_/kf_));
+           currentRho_ = rho();
+//Info << currentRho_ << endl;
+
+           //phi() = (fvc::interpolate(U()) & mesh.Sf());
+
+           }
+
+DD()=U()*runTime().deltaT();
+
+D()=DD()+D().oldTime();
+
+mechanical().grad(D(), gradD());
+// mechanical().grad(U(), gradU());
+
+// Update gradient of displacement increment
+gradDD() = gradD() - gradD().oldTime();  //gradDD()=gradU()*runTime.deltaT()
+
+// Calculate the stress using run-time selectable mechanical law
+mechanical().correct(sigma());
+
+gradU_ = fvc::grad(U());
+
+
+}
+return true;
+}
+
+tmp<vectorField> linGeomPressureVelocitySolid::tractionBoundarySnGrad
+(
+    const vectorField& traction,
+    const scalarField& pressure,
+    const fvPatch& patch
+) const
+{
+    // Patch index
+    const label patchID = patch.index();
+
+    // Patch mechanical property
+    const scalarField& impK = impK_.boundaryField()[patchID];
+
+    // Patch reciprocal implicit stiffness field
+    const scalarField& rImpK = rImpK_.boundaryField()[patchID];
+
+    // Patch gradient
+    const tensorField& pGradU = gradU_.boundaryField()[patchID];
+
+    // Patch stress
+    const symmTensorField& pSigma = sigma().boundaryField()[patchID];
+
+    // Patch unit normals
+    const vectorField n = patch.nf();
+
+    // Return patch snGrad
+    return tmp<vectorField>
+    (
+        new vectorField
+        (
+            (
+                (traction - n*pressure)
+                - (n & (pSigma - runTime().deltaT().value()*impK*pGradU))
+            )*rImpK/runTime().deltaT().value()
+        )
+    );
+}
+
+}
+
+}
+
---
+++ src/solids4FoamModels/solidModels/linGeomVelocitySolid/linGeomVelocitySolid.C
@@ -0,0 +1,247 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright held by original author
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software; you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM; if not, write to the Free Software Foundation,
+    Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+\*---------------------------------------------------------------------------*/
+
+#include "linGeomVelocitySolid.H"
+#include "fvm.H"
+#include "fvc.H"
+#include "fvMatrices.H"
+#include "addToRunTimeSelectionTable.H"
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace Foam
+{
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace solidModels
+{
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+defineTypeNameAndDebug(linGeomVelocitySolid, 0);
+addToRunTimeSelectionTable(physicsModel, linGeomVelocitySolid, solid);
+addToRunTimeSelectionTable(solidModel, linGeomVelocitySolid, dictionary);
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+linGeomVelocitySolid::linGeomVelocitySolid
+(
+    Time& runTime,
+    const word& region
+)
+:
+    solidModel(typeName, runTime, region),
+    impK_(mechanical().impK()),
+    impKf_(mechanical().impKf()),
+    rImpK_(1.0/impK_),
+    gradU_(fvc::grad(U()))
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+
+bool linGeomVelocitySolid::evolve()
+{
+    Info<< "Evolving solid solver" << endl;
+
+    // Mesh update loop
+    do
+    {
+        int iCorr = 0;
+        lduSolverPerformance solverPerfU;
+        blockLduMatrix::debug = 0;
+
+
+
+
+        Info<< "Solving the momentum equation for U" << endl;
+
+
+        // Stablisation viscosity
+        // const dimensionedScalar eta_ =
+        //    solidProperties().lookupOrDefault<dimensionedScalar>
+        //    (
+        //        "numericalViscosity",
+        //        dimensionedScalar("eta", dimless/dimTime, 0.0)
+        //    );
+
+        // Momentum equation loop
+        do
+        {
+            // Store fields for under-relaxation and residual calculation
+            //D().storePrevIter();
+
+
+            U().storePrevIter();
+
+
+            // Linear momentum equation total displacement form
+//            fvVectorMatrix DEqn
+//            (
+//                rho()*fvm::d2dt2(D())
+//                //+ eta_*rho_*fvm::ddt(D())
+//             ==
+//            fvm::laplacian(impKf_, D(), "laplacian(DD,D)")
+//              - fvc::laplacian(impKf_, D(), "laplacian(DD,D)")
+//            + fvc::div(sigma(), "div(sigma)")
+//              + rho()*g()
+////              + mechanical().RhieChowCorrection(D(), gradD())
+//            );
+
+            fvVectorMatrix UEqn
+            (
+                rho()*fvm::ddt(U())
+             == fvm::laplacian(runTime().deltaT()*impKf_, U())
+              - fvc::laplacian(runTime().deltaT()*impKf_, U())
+              + fvc::div(sigma())
+              + rho()*g()
+              + mechanical().RhieChowCorrection
+                (
+                    U(), gradU_, runTime().deltaT()*impKf_
+                )
+            );
+
+//Info<< sigma() << endl;
+            // Under-relaxation the linear system
+            //DEqn.relax();
+
+            UEqn.relax();
+
+            // Solve the linear system
+            //solverPerfD = DEqn.solve();
+
+            solverPerfU = UEqn.solve();
+
+
+
+            // Fixed or adaptive field under-relaxation
+            //relaxField(D(), iCorr);
+            relaxField(U(), iCorr);
+
+
+
+            // Update increment of displacement
+            //DD() = D() - D().oldTime();
+            DD() = U()*runTime().deltaT();
+
+            D() = D().oldTime() + DD();
+
+            // Update gradient of displacement
+            mechanical().grad(D(), gradD());
+           // mechanical().grad(U(), gradU());
+            gradU_ = fvc::grad(U());
+
+            // Update gradient of displacement increment
+            gradDD() = gradD() - gradD().oldTime();
+            //gradDD()=gradU()*runTime.deltaT()
+
+            // Calculate the stress using run-time selectable mechanical law
+            mechanical().correct(sigma());
+//Info<< sigma() << endl;
+
+            // Update impKf to improve convergence
+            // Note: impK and rImpK are not updated as they are used for
+            // traction boundaries
+            //if (iCorr % 10 == 0)
+            //{
+            //    impKf_ = mechanical().impKf();
+            //}
+        }
+        while
+        (
+            !converged
+            (
+                iCorr,
+                solverPerfU.initialResidual(), solverPerfU.nIterations(), U()
+            )
+         && ++iCorr < nCorr()
+        );
+
+        // Interpolate cell displacements to vertices
+        mechanical().interpolate(D(), pointD());
+
+        // Increment of displacement
+        //DD() = D() - D().oldTime();
+        //DD()=U()*mag(runTime().deltaT());
+
+        // Increment of point displacement
+         pointDD() = pointD() - pointD().oldTime();
+
+        // Velocity
+        // U() = fvc::ddt(D());
+
+    }
+    while (mesh().update());
+
+    return true;
+}
+
+tmp<vectorField> linGeomVelocitySolid::tractionBoundarySnGrad
+(
+    const vectorField& traction,
+    const scalarField& pressure,
+    const fvPatch& patch
+) const
+{
+    // Patch index
+    const label patchID = patch.index();
+
+    // Patch mechanical property
+    const scalarField& impK = impK_.boundaryField()[patchID];
+
+    // Patch reciprocal implicit stiffness field
+    const scalarField& rImpK = rImpK_.boundaryField()[patchID];
+
+    // Patch gradient
+    const tensorField& pGradU = gradU_.boundaryField()[patchID];
+
+    // Patch stress
+    const symmTensorField& pSigma = sigma().boundaryField()[patchID];
+
+    // Patch unit normals
+    const vectorField n = patch.nf();
+
+    // Return patch snGrad
+    return tmp<vectorField>
+    (
+        new vectorField
+        (
+            (
+                (traction - n*pressure)
+                - (n & (pSigma - runTime().deltaT().value()*impK*pGradU))
+            )*rImpK/runTime().deltaT().value()
+        )
+    );
+}
+
+}
+
+}
+
